989ee35d6ca5732fb3cf71ac282cd9b6cc6734bc
Igor Dudek <dudekigor@gmail.com>
2020-12-15 19:32:18 +0100

    Added Babel

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, '../', 'index.js'),
+  entry: path.resolve(__dirname, '../', 'src', 'index.js'),
   output: {
     filename: 'js/[name]-[chunkhash].bundle.js',
     path: path.resolve(__dirname, '../', 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            // plugins:
+          },
+        },
+      },
+    ],
+  },
+  plugins: [],
 };
